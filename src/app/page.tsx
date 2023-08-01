0bddcc45608c69821344295a7aad77f362f44316
fazt <fazt@faztweb.com>
2023-08-01 00:10:39 -0500

    added max height to chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,49 +9,49 @@ export default function Chat() {
 
   return (
     <section className="flex justify-center items-center h-screen">
-      <div className="text-white max-w-xl w-full h-4/5 overflow-y-auto">
-        {messages.map((m) => (
-          <div
-            key={m.id}
-            className={`flex flex-col mb-2 p-2 rounded-md ${
-              m.role === "assistant"
-                ? "self-end bg-gray-800"
-                : "self-start bg-blue-700"
-            }`}
-          >
-            <span
-              className={`text-xs ${
-                m.role === "assistant" ? "text-right" : "text-left"
+      <form onSubmit={handleSubmit} className="max-w-xl w-full">
+        <div className="text-white max-h-96 h-full overflow-y-auto">
+          {messages.map((m) => (
+            <div
+              key={m.id}
+              className={`flex flex-col mb-2 p-2 rounded-md ${
+                m.role === "assistant"
+                  ? "self-end bg-gray-800"
+                  : "self-start bg-blue-700"
               }`}
             >
-              {m.role}
-            </span>{" "}
-            {m.content}
-          </div>
-        ))}
+              <span
+                className={`text-xs ${
+                  m.role === "assistant" ? "text-right" : "text-left"
+                }`}
+              >
+                {m.role}
+              </span>{" "}
+              {m.content}
+            </div>
+          ))}
+        </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="flex justify-between my-4">
-            <label className="text-white block font-bold my-2">
-              Say something...
-            </label>
-            <button
-              className="bg-blue-600 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:opacity-50"
-              disabled={isLoading || !input}
-            >
-              Send
-            </button>
-          </div>
-          <textarea
-            rows={4}
-            value={input}
-            onChange={handleInputChange}
-            className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
-            placeholder="Type something..."
-            autoFocus
-          />
-        </form>
-      </div>
+        <div className="flex justify-between my-4">
+          <label className="text-white block font-bold my-2">
+            Say something...
+          </label>
+          <button
+            className="bg-blue-600 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 disabled:opacity-50"
+            disabled={isLoading || !input}
+          >
+            Send
+          </button>
+        </div>
+        <textarea
+          rows={4}
+          value={input}
+          onChange={handleInputChange}
+          className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+          placeholder="Type something..."
+          autoFocus
+        />
+      </form>
     </section>
   );
 }
